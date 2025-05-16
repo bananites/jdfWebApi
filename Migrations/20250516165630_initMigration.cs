4d7a97d53ff0ac8855e1ee2c6c381f60dd2f166d
Bananonites <bananonites@bananas.com>
2025-05-16 19:11:47 +0200

    run init migration

---
+++ Migrations/20250516165630_initMigration.cs
@@ -0,0 +1,160 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataPollingApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class initMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "job",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int(11)", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    xmlPath = table.Column<string>(type: "varchar(200)", maxLength: 200, nullable: true, collation: "utf8mb4_general_ci")
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PRIMARY", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .Annotation("Relational:Collation", "utf8mb4_general_ci");
+
+            migrationBuilder.CreateTable(
+                name: "machine",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int(11)", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    type = table.Column<string>(type: "varchar(40)", maxLength: 40, nullable: true, collation: "utf8mb4_general_ci")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    yearBuilt = table.Column<int>(type: "int(11)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PRIMARY", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .Annotation("Relational:Collation", "utf8mb4_general_ci");
+
+            migrationBuilder.CreateTable(
+                name: "user",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int(11)", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    username = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: true, collation: "utf8mb4_general_ci")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    password = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: true, collation: "utf8mb4_general_ci")
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PRIMARY", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .Annotation("Relational:Collation", "utf8mb4_general_ci");
+
+            migrationBuilder.CreateTable(
+                name: "machine_jobs",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int(11)", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    machineId = table.Column<int>(type: "int(11)", nullable: true),
+                    jobId = table.Column<int>(type: "int(11)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PRIMARY", x => x.id);
+                    table.ForeignKey(
+                        name: "machine_jobs_ibfk_1",
+                        column: x => x.machineId,
+                        principalTable: "machine",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "machine_jobs_ibfk_2",
+                        column: x => x.jobId,
+                        principalTable: "job",
+                        principalColumn: "id");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .Annotation("Relational:Collation", "utf8mb4_general_ci");
+
+            migrationBuilder.CreateTable(
+                name: "user_job",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int(11)", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    jobId = table.Column<int>(type: "int(11)", nullable: true),
+                    userId = table.Column<int>(type: "int(11)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PRIMARY", x => x.id);
+                    table.ForeignKey(
+                        name: "user_job_ibfk_1",
+                        column: x => x.jobId,
+                        principalTable: "job",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "user_job_ibfk_2",
+                        column: x => x.userId,
+                        principalTable: "user",
+                        principalColumn: "id");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .Annotation("Relational:Collation", "utf8mb4_general_ci");
+
+            migrationBuilder.CreateIndex(
+                name: "jobId",
+                table: "machine_jobs",
+                column: "jobId");
+
+            migrationBuilder.CreateIndex(
+                name: "machineId",
+                table: "machine_jobs",
+                column: "machineId");
+
+            migrationBuilder.CreateIndex(
+                name: "jobId1",
+                table: "user_job",
+                column: "jobId");
+
+            migrationBuilder.CreateIndex(
+                name: "userId",
+                table: "user_job",
+                column: "userId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "machine_jobs");
+
+            migrationBuilder.DropTable(
+                name: "user_job");
+
+            migrationBuilder.DropTable(
+                name: "machine");
+
+            migrationBuilder.DropTable(
+                name: "job");
+
+            migrationBuilder.DropTable(
+                name: "user");
+        }
+    }
+}
