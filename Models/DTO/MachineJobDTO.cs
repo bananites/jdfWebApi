b27e1716ec9daefce8d9275d9926203fbe7db57f
Bananonites <bananonites@bananas.com>
2025-05-17 19:38:42 +0200

    add a Job obj to machineJobDTO

--- Models/DTO/MachineJobDTO.cs
+++ Models/DTO/MachineJobDTO.cs
@@ -1,6 +1,10 @@
+using DataPollingApi.Models;
+
 public class MachineJobDTO
 {
     public int Id { get; set; }
     public int? MachineId { get; set; }
     public int? JobId { get; set; }
+
+    public Job? Job { get; set; }
 }
