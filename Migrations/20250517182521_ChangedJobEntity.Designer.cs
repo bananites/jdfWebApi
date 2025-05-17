a5c794e0eb1a659f2d2254cc48a4583a41d7920d
Bananonites <bananonites@bananas.com>
2025-05-17 20:45:40 +0200

    added new migration, changed Job Entity

--- Migrations/20250517182521_ChangedJobEntity.Designer.cs
+++ Migrations/20250517182521_ChangedJobEntity.Designer.cs
@@ -4,6 +4,7 @@ using DataPollingApi.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataPollingApi.Migrations
 {
     [DbContext(typeof(S31JdfMachineHandlerContext))]
-    partial class S31JdfMachineHandlerContextModelSnapshot : ModelSnapshot
+    [Migration("20250517182521_ChangedJobEntity")]
+    partial class ChangedJobEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20250517182521_ChangedJobEntity.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataPollingApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedJobEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "name",
+                table: "job",
+                type: "longtext",
+                nullable: true,
+                collation: "utf8mb4_general_ci")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "status",
+                table: "job",
+                type: "longtext",
+                nullable: true,
+                collation: "utf8mb4_general_ci")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "name",
+                table: "job");
+
+            migrationBuilder.DropColumn(
+                name: "status",
+                table: "job");
+        }
+    }
+}
