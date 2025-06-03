86623d8730e8e881133e8871b4a0655ffa1b25b6
bananites <pascal@MOBIL-109.steuber.local>
2025-06-03 08:53:37 +0200

    when writing null we dont need the UserJobs

--- Models/DTO/UserDTO.cs
+++ Models/DTO/UserDTO.cs
@@ -1,7 +1,10 @@
+using System.Text.Json.Serialization;
+
 public class UserDTO
 {
     public int Id { get; set; }
     public string? Username { get; set; }
-    
+
+   [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)] 
     public List<JobDTO>? UserJobs { get; set; }
 }
